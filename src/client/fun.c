b569438e655733242c2d84afc1d7440c4edf8277
Dimitri Fontaine <dim@tapoueh.org>
2014-03-13 16:24:34 +0100

    Implement pginstall_platform() function in the extension.

--- src/client/fun.c
+++ src/client/fun.c
@@ -18,7 +18,72 @@
 #include "utils/builtins.h"
 #include "utils/memutils.h"
 
+Datum       pginstall_platform(PG_FUNCTION_ARGS);
 Datum       pginstall_available_extensions(PG_FUNCTION_ARGS);
+
+PG_FUNCTION_INFO_V1(pginstall_platform);
+
+Datum
+pginstall_platform(PG_FUNCTION_ARGS)
+{
+    ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
+    TupleDesc   tupdesc;
+    Tuplestorestate *tupstore;
+    MemoryContext per_query_ctx;
+    MemoryContext oldcontext;
+    PlatformData platform;
+	Datum       values[3];
+	bool        nulls[3];
+
+    /* Fetch the list of available extensions */
+    current_platform(&platform);
+
+    /* check to see if caller supports us returning a tuplestore */
+    if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
+        ereport(ERROR,
+                (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                 errmsg("set-valued function called in context that cannot accept a set")));
+    if (!(rsinfo->allowedModes & SFRM_Materialize))
+        ereport(ERROR,
+                (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                 errmsg("materialize mode required, but it is not " \
+                        "allowed in this context")));
+
+    /* Build a tuple descriptor for our result type */
+    if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+        elog(ERROR, "return type must be a row type");
+
+    /* Build tuplestore to hold the result rows */
+    per_query_ctx = rsinfo->econtext->ecxt_per_query_memory;
+    oldcontext = MemoryContextSwitchTo(per_query_ctx);
+
+    tupstore = tuplestore_begin_heap(true, false, work_mem);
+    rsinfo->returnMode = SFRM_Materialize;
+    rsinfo->setResult = tupstore;
+    rsinfo->setDesc = tupdesc;
+
+    MemoryContextSwitchTo(oldcontext);
+
+	memset(values, 0, sizeof(values));
+	memset(nulls, 0, sizeof(nulls));
+
+	/* os name */
+	values[0] = CStringGetTextDatum(platform.os_name);
+
+	/* os version */
+	values[1] = CStringGetTextDatum(platform.os_version);
+
+	/* arch */
+	values[2] = CStringGetTextDatum(platform.arch);
+
+	tuplestore_putvalues(tupstore, tupdesc, values, nulls);
+
+	/* clean up and return the tuplestore */
+    tuplestore_donestoring(tupstore);
+
+    return (Datum) 0;
+
+}
 
 PG_FUNCTION_INFO_V1(pginstall_available_extensions);
 
@@ -100,3 +165,4 @@ pginstall_available_extensions(PG_FUNCTION_ARGS)
 
     return (Datum) 0;
 }
+
--- src/client/pginstall--1.0.sql
+++ src/client/pginstall--1.0.sql
@@ -1,6 +1,15 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pginstall" to load this file. \quit
 
+CREATE OR REPLACE FUNCTION pginstall_platform(
+  OUT os_name     text,
+  OUT os_version  text,
+  OUT arch        text
+)
+RETURNS SETOF record
+AS 'pginstall'
+LANGUAGE C IMMUTABLE STRICT;
+
 CREATE OR REPLACE FUNCTION pginstall_available_extensions(
   OUT id          bigint,
   OUT shortname   text,
