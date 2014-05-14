9f1dac44384fa2a25f773f42d9f0cc4f5a882a0c
Dimitri Fontaine <dim@tapoueh.org>
2014-05-14 23:54:19 +0200

    Add more PL/pgSQL API into pginstall setup.

--- src/repo/api.sql
+++ src/repo/api.sql
@@ -2,7 +2,11 @@
 -- Implement the data access as a set of Stored Procedure
 -- See each function in the relevant file.
 --
+
+\i sql/archive_pathname.sql
 \i sql/cancel_build.sql
 \i sql/list_build_queue.sql
+\i sql/list_extensions_for.sql
 \i sql/pick_from_queue.sql
 \i sql/queue_build.sql
+\i sql/register_build.sql
